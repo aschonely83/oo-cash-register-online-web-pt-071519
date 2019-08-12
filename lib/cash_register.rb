388cc469c2e4ab3c702e647f96f7560c6ec1ad03
Adam Schonely <aschonely@gmail.com>
2019-08-12 23:47:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,10 @@
 class CashRegister
   attr_accessor :total, :discount, :last_transaction_amount, :item 
   
-  def initialize(item = 0)
-    @total = total
+  def initialize(discount = 0)
+    @total = 0
     @discount = discount
-    
+    item = []
   end  
   
 end
