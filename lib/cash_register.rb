5cb5fc4a2e766a7ddf5b2d5be485de138a02e6c5
Adam Schonely <aschonely@gmail.com>
2019-08-13 00:43:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,6 +19,7 @@ class CashRegister
   def apply_discount
     if discount != 0 
       self.total(total * ((100.0 - discount.to_f/100)).to_i 
+      "After "
   end
   
   def void_last_transaction
